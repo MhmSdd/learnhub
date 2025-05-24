b7c0b90c6030fdf86846dea1ecc9502f415a300a
mohamad Said <a.windows.z@gmail.com>
2025-05-24 21:36:09 +0200

    Update config to use DATABASE_URL from env

--- config.py
+++ config.py
@@ -3,11 +3,11 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_COOKIE_SAMESITE= 'Lax'
     SESSION_COOKIE_SECURE= False 
-    SECRET_KEY = 'X7b9kL2mQz8pWvT3rYcN5jF0hG4iA9oD_eBxU6nM'
+    SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key")
     LANGUAGES = {
         'en': 'English',
         'ar': 'Arabic'
